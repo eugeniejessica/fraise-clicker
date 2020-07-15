2b148f42eaa978082a9a79ac3aec26be5ca5c944
fbemb <f.bembassat@gmail.com>
2020-07-15 16:34:15 +0200

    modification js

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ document.getElementById("buttonFraise").addEventListener("click", () => {
     
     counter = counter + fraise;
     document.getElementById("counter").innerHTML = counter;
+
 });
+
+document.getElementById("multiplier").addEventListener("click", () => {
+
+    fraise = fraise * 2;
+
+});
+
+
+document.getElementById("autoclicker").addEventListener("click", () => {
+
+    setInterval(function(){ counter = counter + 1;document.getElementById("counter").innerHTML = counter;}, 1000)
+    
+});
+
