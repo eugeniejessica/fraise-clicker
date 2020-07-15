e39728a1d7855e8db494e03d9fe687cb5dffe598
Olivier Peeters <olivierpeeters73@gmai.com>
2020-07-15 22:42:35 +0200

    Bonus button JS

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ document.getElementById("autoclicker").addEventListener("click", () => {
     
 });
 
+document.getElementById("bonus").addEventListener("click", () => {
+
+    let d1 = new Date();
+    fraise = fraise * 100;
+    let startBonus = setInterval(counterBonus, 1000);
+    function counterBonus() {
+        let d2 = new Date();
+        let dateDiff = d2 - d1;
+        if (dateDiff > 30000) {
+            fraise = fraise / 100;
+            clearInterval(startBonus);
+        };
+
+    };
+    
+});
+
